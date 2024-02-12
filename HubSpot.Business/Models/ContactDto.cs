cb4995a53a4932992fe77ba9c52eaf05bab589a3
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2024-02-12 17:00:11 -0600

    Created ContactDto which I will use to Map the API Response into

---
+++ HubSpot.Business/Models/ContactDto.cs
@@ -0,0 +1,13 @@
+﻿namespace HubSpot.Business.Models
+{
+    /// <summary>
+    /// Represents One Contact => Generated from the HubSpotApiResponse
+    /// </summary>
+    public class ContactDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
