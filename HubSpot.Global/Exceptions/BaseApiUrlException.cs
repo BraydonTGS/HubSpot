dd92c254da74ec9b5e811a3959d14b9afca9f061
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2024-02-12 15:58:11 -0600

    Created ApiException and BaseApiUrlException

---
+++ HubSpot.Global/Exceptions/BaseApiUrlException.cs
@@ -0,0 +1,12 @@
+﻿namespace HubSpot.Global.Exceptions
+{
+    /// <summary>
+    /// Custom Exception for when there is an issue setting the BaseApiUrl
+    /// </summary>
+    public class BaseApiUrlException : Exception
+    {
+        public BaseApiUrlException(string message) : base(message) { }
+
+        public BaseApiUrlException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
