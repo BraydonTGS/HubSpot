c16ac2575460a249fea0476fb01ee9fd128fc3ec
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2024-02-12 16:09:14 -0600

    Created Test Project for the Business Project and Created Test Base for Registering Dependencies

---
+++ HubSpot.Business.Tests/Base/TestBase.cs
@@ -0,0 +1,21 @@
+﻿using HubSpot.Business.Api;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HubSpot.Business.Tests.Base
+{
+    public class TestBase
+    {
+        // Register Services Required for Unit Tests //
+        public virtual IServiceCollection ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            // Api Services //
+            services.AddScoped<HttpClient>();
+            services.AddScoped<IApiService, ApiService>();
+            services.AddScoped<IHubSpotApiService, HubSpotApiService>();
+
+            return services;
+        }
+    }
+}
