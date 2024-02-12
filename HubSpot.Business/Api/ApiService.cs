775513053638232c3f4aa0404b6fa246bb5fe1a5
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2024-02-12 15:57:47 -0600

    Created IApiService and ApiService. IApiService will Define the requirements for the ApiService to implement. Used to Setting the ApiUrl and Adding Authorization Headers. One Method GetApiResponseAsync which is generic of type TResponse

---
+++ HubSpot.Business/Api/ApiService.cs
@@ -0,0 +1,63 @@
+﻿using HubSpot.Business.Connection;
+using HubSpot.Global.Constants;
+using HubSpot.Global.Exceptions;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace HubSpot.Business.Api
+{
+    /// <summary>
+    /// ApiService 
+    /// </summary>
+    public class ApiService : IApiService
+    {
+        private string _baseApiUrl { get; set; } = string.Empty;
+        private readonly HttpClient _client;
+
+        public ApiService(HttpClient client) => _client = client ?? throw new ArgumentNullException(nameof(client));
+
+        #region SetBaseApiUrl
+        public void SetBaseApiUrl(string baseApiUrl)
+        {
+            if (string.IsNullOrEmpty(baseApiUrl)) { throw new BaseApiUrlException("The Base API URL can not be Null or Empty"); }
+            _baseApiUrl = baseApiUrl;
+        }
+        #endregion
+
+        #region AddBearerToken
+        public void SetAuthorizationHeader(string token)
+        {
+            if (_client is not null)
+                _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(Constants.Bearer, token);
+        }
+        #endregion
+
+        #region GetApiResponseAsync
+        public async Task<TResponse> GetApiResponseAsync<TResponse>(string url)
+        {
+            try
+            {
+                var response = await _client.GetStringAsync(url);
+
+                var results = JsonConvert.DeserializeObject<TResponse>(response);
+
+                if (results is null) return Activator.CreateInstance<TResponse>();
+
+                return results;
+            }
+            catch (Exception ex)
+            {
+                if (ex.GetType() == typeof(JsonSerializationException))
+                {
+                    throw new JsonException("An error occurred while deserializing the API response.", ex);
+                }
+                else
+                    throw new ApiException("An error occurred while processing the request.", ex);
+            }
+        }
+        #endregion
+
+
+
+    }
+}
---
+++ HubSpot.Business/Api/IApiService.cs
@@ -0,0 +1,12 @@
+﻿namespace HubSpot.Business.Api
+{
+    /// <summary>
+    /// Interface used to define functionality that will be implemented by <see cref=ApiService/>
+    /// </summary>
+    public interface IApiService
+    {
+        public void SetBaseApiUrl(string baseApiUrl);
+        public void SetAuthorizationHeader(string token);   
+        public Task<TResponse> GetApiResponseAsync<TResponse>(string url);
+    }
+}
