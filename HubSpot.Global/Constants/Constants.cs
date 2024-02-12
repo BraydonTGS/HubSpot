3ae1c5023902d416672b49368449bbb83703cc6e
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2024-02-12 15:59:00 -0600

    Added Project Reference to Global and a Constants Class for Storing Constant Strings

---
+++ HubSpot.Global/Constants/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace HubSpot.Global.Constants
+{
+    public class Constants
+    {
+        #region API
+        public const string NotAvailable = "N/A";
+        public const string HubSpotBaseUrl = "https://api.hubapi.com/";
+        public const string Bearer = "Bearer";
+        #endregion
+    }
+}
