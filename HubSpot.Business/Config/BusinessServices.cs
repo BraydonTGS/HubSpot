b7a404d32177b6cfdb6dd623d2294a9174452d85
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2024-02-12 15:58:32 -0600

    Created a BusinessServices Class for Registering Dependencies within the Business Project

---
+++ HubSpot.Business/Config/BusinessServices.cs
@@ -0,0 +1,22 @@
+﻿using HubSpot.Business.Api;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HubSpot.Business.Config
+{
+    public static class BusinessServices
+    {
+        /// <summary>
+        /// Register any Dependencies Needed for the Business Project
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection ConfigureBusinessServices(IServiceCollection services)
+        {
+            // Services //
+            services.AddScoped<HttpClient>();
+            services.AddScoped<IApiService, ApiService>();
+
+            return services;
+        }
+    }
+}
