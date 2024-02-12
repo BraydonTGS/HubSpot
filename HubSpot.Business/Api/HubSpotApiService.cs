a297d6eb08d5a9a02893246497adf8469df18f7b
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2024-02-12 16:09:34 -0600

    Created HubSpotApiService and IHubSpotApiService

---
+++ HubSpot.Business/Api/HubSpotApiService.cs
@@ -0,0 +1,30 @@
+﻿using HubSpot.Business.Connection;
+using HubSpot.Global.Constants;
+
+namespace HubSpot.Business.Api
+{
+    /// <summary>
+    /// HubSpotApiService Class 
+    /// 
+    /// Used for interacting with the HubSpotApi
+    /// </summary>
+    public class HubSpotApiService : IHubSpotApiService
+        {
+        private readonly IApiService _apiService;
+
+        public HubSpotApiService(IApiService apiService)
+        {
+            _apiService = apiService;
+            _apiService.SetBaseApiUrl(Constants.HubSpotBaseUrl);
+            _apiService.SetAuthorizationHeader(Hidden.GetHubSpotApiKey());
+        }
+
+        #region GetSpecifiedListOfContactsAsync
+        public async Task GetSpecifiedListOfContactsAsync(int id)
+        {
+            return;
+        }
+        #endregion
+
+    }
+}
---
+++ HubSpot.Business/Api/IHubSpotApiService.cs
@@ -0,0 +1,11 @@
+﻿
+namespace HubSpot.Business.Api
+{
+    /// <summary>
+    /// Interface to Define the Implementation for the <see cref="HubSpotApiService"/>
+    /// </summary>
+    public interface IHubSpotApiService
+    {
+        Task GetSpecifiedListOfContactsAsync(int listId);
+    }
+}
