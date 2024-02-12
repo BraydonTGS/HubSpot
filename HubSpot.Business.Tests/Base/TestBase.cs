dadaaecc585307067d3ae0fce9b17e8ba990968b
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2024-02-12 16:26:35 -0600

    Created TestClass for HubSpotApiService, Added First Test, Ctor Not Null Success, DI working as expected

--- HubSpot.Business.Tests/Base/TestBase.cs
+++ HubSpot.Business.Tests/Base/TestBase.cs
@@ -1,8 +1,13 @@
 ﻿using HubSpot.Business.Api;
 using Microsoft.Extensions.DependencyInjection;
 
+
 namespace HubSpot.Business.Tests.Base
 {
+    /// <summary>
+    /// Base Class for any Business Logic Test Classes
+    /// Used for Configuring Dependencies and Seeding Data if Needed
+    /// </summary>
     public class TestBase
     {
         // Register Services Required for Unit Tests //
---
+++ HubSpot.Business.Tests/HubSpotApiServiceTest/HubSpotApiServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using HubSpot.Business.Api;
+using HubSpot.Business.Tests.Base;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HubSpot.Business.Tests.HubSpotApiTest
+{
+    [TestClass]
+    public class HubSpotApiServiceTests : TestBase
+    {
+        private readonly IServiceCollection _services;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly IHubSpotApiService _hubSpotApiService;
+
+        public HubSpotApiServiceTests()
+        {
+            _services = ConfigureServices();
+            _serviceProvider = _services.BuildServiceProvider();
+            _hubSpotApiService = _serviceProvider.GetRequiredService<IHubSpotApiService>();
+        }
+
+        [TestMethod]
+        public void ConstructorNotNull_Success()
+        {
+            Assert.IsNotNull(_hubSpotApiService);
+        }
+    }
+}
