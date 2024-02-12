d49278fd0a93ae2d4adae4f9a759a4218e0dd089
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2024-02-12 17:55:34 -0600

    Added Debugger Display

--- HubSpot.Business/Models/ContactDto.cs
+++ HubSpot.Business/Models/ContactDto.cs
@@ -1,13 +1,22 @@
-﻿namespace HubSpot.Business.Models
+﻿using System.Diagnostics;
+
+namespace HubSpot.Business.Models
 {
     /// <summary>
     /// Represents One Contact => Generated from the HubSpotApiResponse
     /// </summary>
+    [DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
     public class ContactDto
     {
         public int Id { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+        public string AmsMemberNumber {  get; set; } = string.Empty;
+
+        private string GetDebuggerDisplay()
+        {
+            return $"Id: {Id}, FirstName: {FirstName}, LastName: {LastName}";
+        }
     }
 }
