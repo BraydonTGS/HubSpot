a850bf313b5bbce57a1b3cdd37cfa57c0665ca64
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2024-02-13 06:02:00 -0600

    UnIgnored Hidden File for easier set up when Cloning

---
+++ HubSpot.Business/Connection/Hidden.cs
@@ -0,0 +1,41 @@
+﻿namespace HubSpot.Business.Connection
+{
+    /// <summary>
+    /// Hidden File
+    /// 
+    /// Connection Strings
+    /// 
+    /// URLs
+    /// 
+    /// API Keys
+    /// 
+    ///Git Ignore This File After You Clone
+    /// </summary>
+    public static class Hidden
+    {
+        private static string _connectionString { get; set; } = "";
+
+        /// <summary>
+        /// Add the HubSpotApi Key Here
+        /// </summary>
+        private static string _hubSpotApiKey { get; set; } = "";
+
+        /// <summary>
+        /// Get the DataBase Connection String
+        /// </summary>
+        /// <returns></returns>
+        public static string GetConnectionString()
+        {
+            return _connectionString;
+        }
+
+        /// <summary>
+        /// Used to get the Specified API Key if Applicable. 
+        /// </summary>
+        /// <returns></returns>
+        public static string GetHubSpotApiKey()
+        {
+            return _hubSpotApiKey;
+        }
+    }
+}
